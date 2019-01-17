89b5127ef8e9364179b5909d7bf14c4c3c18bbe8
Chengyin Liu <liuchengyin1234@126.com>
2019-01-16 16:22:30 -0800

    .

---
+++ Algorithems/Sort/Bubble Sort/C++/BubbleSort/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+template<typename T>
+void Bubblesort(T *array, int n){
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if(array[j] > array[j + 1]){
+                T temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+}
+
+template<typename T>
+void printarray(T array[], int n){
+    for(int i = 0; i < n; i++){
+        cout<<array[i]<<" ";
+    }
+    cout << endl;
+}
+
+int a[] = {54,6,7,33,2,66,8};
+float b[] = {5.4, 6.7, 9.0, 4.0, 14.6, 100.5, 40.7};
+
+int main(){
+    int na = sizeof(a) / sizeof(a[0]);
+    int nb = sizeof(b) / sizeof(*b);
+    Bubblesort(a, na);
+    printarray(a, na);
+    Bubblesort(b, nb);
+    printarray(b, nb);
+    return 0;
+}
+
--- Algorithems/Sort/try.cpp
+++
@@ -1,3 +0,0 @@
-int a
-a = 1
-print a
