bb75e9f8736c78ab2375dc974f1b6d28fa595d71
lynkeib <liuchengyin1234@126.com>
2020-08-01 21:45:40 -0700

    Create 359. Logger Rate Limiter.java

---
+++ ByTags/Binary Search/Java/359. Logger Rate Limiter.java
@@ -0,0 +1,33 @@
+class Logger {
+
+    Map<String, Integer> log = new HashMap();
+
+    /** Initialize your data structure here. */
+    public Logger() {
+
+    }
+
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+     If this method returns false, the message will not be printed.
+     The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(log.containsKey(message)){
+            int lastTime = log.get(message);
+            if(lastTime + 10 <= timestamp){
+                log.put(message, timestamp);
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            log.put(message, timestamp);
+            return true;
+        }
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
