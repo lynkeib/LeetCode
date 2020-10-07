49e05ef7da97e2f9e3c92279872ba0af4b2faa7e
lynkeib <liuchengyin1234@126.com>
2020-10-06 19:26:56 -0700

    Create 825. Friends Of Appropriate Ages.py

---
+++ ByTags/Others/825. Friends Of Appropriate Ages.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def numFriendRequests(self, ages):
+        """
+        :type ages: List[int]
+        :rtype: int
+        """
+        counter = dict()
+        for age in ages:
+            if age not in counter:
+                counter[age] = 0
+            counter[age] += 1
+
+        res = 0
+        for key, value in counter.items():
+            for otherAge in counter:
+                if 0.5 * key + 7 < otherAge and key >= otherAge and (key >= 100 or otherAge <= 100):
+                    res += value * counter[otherAge]
+                    if key == otherAge:
+                        res -= value
+        return res
